b0c08c18e1a6be93e09c12224f84f60b1ea2c287
Nguyen Quoc <quocnguyen612k@gmail.com>
2021-01-04 00:42:16 +0700

    Add feature add scores

--- StudentManagement/mainapp.py
+++ StudentManagement/mainapp.py
@@ -8,7 +8,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 @app.route("/")
 @login_required
 def index():
-    return render_template("/TeachingManager/index.html")
+    return render_template("/base/index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -41,6 +41,7 @@ def login():
         # Remember which user has logged in
         session["user_id"] = account.id
         session["username"] = account.username
+        session["role"] = account.role
         # flask notice message
         flash('Welcome ' + account.username)
 
@@ -205,35 +206,179 @@ def updateSubjectNameRegulation():
     return redirect("/educationregulations")
 
 
-@app.route("/managestudent")
-@login_required
-def addStudentPage():
-    """đi đến trang tiếp nhận học sinh"""
-    return render_template("TeachingManager/add student.html")
-
-@app.route("/managestudent/addstudent" , methods=["POST"])
+@app.route("/managestudent/addstudent" , methods=["GET", "POST"])
 @login_required
 def addStudent():
-    """Thêm học sinh mới"""
+    """Thêm tiếp nhận học sinh """
 
-    # lấy request thông tin học sinh
-    firstname = request.form.get("firstname")
-    lastname = request.form.get("lastname")
-    gender = request.form.get("gender")
-    email = request.form.get("email")
-    birthdate = request.form.get("birthday")
-    address = request.form.get("address")
+    if request.method == 'GET':
+        #đi đến trang tiếp nhận học sinh
+        return render_template("TeachingManager/add student.html")
+    else:
+        # lấy request thông tin học sinh
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        gender = request.form.get("gender")
+        email = request.form.get("email")
+        birthdate = request.form.get("birthday")
+        address = request.form.get("address")
 
-    # thêm học sinh vào csdl
-    student = Students(firstname=firstname,lastname=lastname,gender=gender,address=address,email=email,birthdate=birthdate)
-    db.session.add(student)
-    db.session.commit()
+        # thêm học sinh vào csdl
+        student = Students(firstname=firstname,lastname=lastname,gender=gender,address=address,email=email,birthdate=birthdate)
+        db.session.add(student)
+        db.session.commit()
 
-    # flask notice message
-    flash('HỌc sinh '+lastname+' '+firstname+' đã được tiếp nhận thành công!')
-    return redirect("/managestudent")
+        # flask notice message
+        flash('HỌc sinh '+lastname+' '+firstname+' đã được tiếp nhận thành công!')
+        return redirect("/managestudent/addstudent")
 
 
+@app.route("/managestudent/searchstudent", methods=["GET", "POST"])
+@login_required
+def searchStudent():
+    """tìm kiếm học sinh"""
+    students = Students.query.all()
+    return render_template("base/search student.html", students=students)
+
+
+@app.route("/managestudent/sortstudent", methods=["GET", "POST"])
+@login_required
+def sortStudent():
+    """xếp lớp cho học sinh"""
+    # lấy danh sách các khối
+    grades = Grade.query.all()
+    # lấy danh sách các lớp
+    classes = Classes.query.all()
+    # danh sách học sinh cần xếp lớp khối 10
+    unsorted10 = Students.query.filter_by(year_id=1, class_id=None)
+    # danh sách học sinh cần xếp lớp khối 11
+    unsorted11 = Students.query.filter_by(year_id=2, class_id=None)
+    # danh sách học sinh cần xếp lớp khối 12
+    unsorted12 = Students.query.filter_by(year_id=3, class_id=None)
+
+    if request.method == 'GET':
+        # danh sách học sinh
+        students = Students.query.all()
+
+        return render_template("TeachingManager/sort student.html", students=students, unsorted10=unsorted10,unsorted11=unsorted11,unsorted12=unsorted12, grades=grades, classes=classes)
+    else:
+        #láy khối cần xếp lớp
+        sort_grade = request.form.get('grade')
+        student_list = []
+        if sort_grade == '10':
+            # lấy danh sách các lớp theo khối
+            classes = Classes.query.filter_by(grade=1)
+            student_list = unsorted10
+        elif sort_grade == '11':
+            # lấy danh sách các lớp theo khối
+            classes = Classes.query.filter_by(grade=2)
+            student_list = unsorted11
+        else:
+            # lấy danh sách các lớp theo khối
+            classes = Classes.query.filter_by(grade=3)
+            student_list = unsorted11
+        print(classes)
+        # xếp lớp cho các lớp theo khối
+        num_class = classes.count()
+        index = 0
+        for student in student_list:
+            if index <= num_class -1:
+                student.class_id = classes[index].class_id
+                db.session.commit()
+                index += 1
+                if index == num_class:
+                    index = 0
+
+        # flask notice message
+        flash('Hệ thống đã tự động xếp lớp cho khối '+sort_grade)
+
+        return  redirect('/managestudent/sortstudent')
+
+
+@app.route("/manageaccount/adduser", methods=["GET", "POST"])
+@login_required
+def addUser():
+    """thêm user mới """
+    if request.method == 'GET':
+        return render_template("TeachingManager/add user.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        name = request.form.get("name")
+        role = request.form.get("role")
+
+        account = Account(username=username, password=generate_password_hash(password), role=role, name=name)
+        db.session.add(account)
+        db.session.commit()
+
+        # flask notice message
+        flash('Tài khoản ' + username + ' đã được tạo thành công!')
+        return redirect("/manageaccount/adduser")
+
+
+@app.route("/managestudent/scoreinfo", methods=["GET","POST"])
+@login_required
+def infoScore():
+    """hiển thị thông tin nhập điểm"""
+    if request.method == 'GET':
+        # lấy thông tin giáo viên
+        teacher = Teachers.query.filter_by(account_id=session["user_id"]).first()
+        # lấy teacher_id
+        teacher_id = teacher.teacher_id
+        #lấy các lớp giáo viên được phân công dạy
+        assignments = Assignments.query.filter_by(teacher_id=teacher_id)
+        #lấy học kì
+        semesters = Semesters.query.all()
+        #lấy năm học
+        school_years = School_year.query.all()
+
+        return render_template("Teacher/score info.html",teacher=teacher,assignments=assignments,semesters=semesters,school_years=school_years)
+    else:
+        #lấy loại điểm
+        score_type = request.form.get('score_type')
+        class_id = request.form.get('class_id')
+        semester_id = request.form.get('semester_id')
+        year_id = request.form.get('year_id')
+
+        session['score_type'] = score_type
+        session['class_id'] = class_id
+        session['semester_id'] = semester_id
+        session['year_id'] = year_id
+        return redirect("/managestudent/addscore")
+
+@app.route("/managestudent/addscore")
+@login_required
+def addScore():
+    """hiển thị danh sách nhập điểm"""
+    if request.method == 'GET':
+        # lấy thông tin giáo viên
+        teacher = Teachers.query.filter_by(account_id=session["user_id"]).first()
+        # lấy teacher_id
+        teacher_id = teacher.teacher_id
+        # lấy id môn học
+        subject_id = teacher.subject_id
+        # lấy thông tin bảng điểm
+        score_board = ScoreBoard.query.filter_by(teacher_id=teacher_id,subject_id=subject_id,school_year_id=session['year_id'],semester_id=session['semester_id'])
+        # lấy lớp cần nhập điểm
+        class_ = Classes.query.filter_by(class_id=session["class_id"]).first()
+        # lấy danh sách học sinh theo lớp
+        students = Students.query.filter_by(class_id=session["class_id"]).all()
+
+        # nếu chưa có bảng điểm
+        if score_board.count() == 0:
+            #tạo bảng điểm của môn học
+            for student in students:
+                board = ScoreBoard(student_id=student.student_id,teacher_id=teacher_id,subject_id=subject_id,school_year_id=session['year_id'],semester_id=session['semester_id'])
+                db.session.add(board)
+                db.session.commit()
+            #lấy lại thông tin bản điểm
+            score_board = ScoreBoard.query.filter_by(teacher_id=teacher_id, subject_id=subject_id,
+                                                         school_year_id=session['year_id'],
+                                                         semester_id=session['semester_id']).all()
+        # lấy điểm theo loại
+        scores = Score.query.filter_by(type_id=session['score_type'])
+
+        return render_template("Teacher/add score.html",teacher=teacher,students=students,score_board=score_board,scores=scores,class_=class_)
 
 
 if __name__ == '__main__':
--- StudentManagement/models.py
+++ StudentManagement/models.py
@@ -37,6 +37,16 @@ class Classes(db.Model):
     def __str__(self):
         return self.name
 
+class EntryYear(db.Model):
+    __tablename__ = 'entry_year'
+
+    year_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    grade = Column(Integer, ForeignKey(Grade.grade_id), nullable=False)
+    grade_name = db.relationship('Grade', backref=db.backref('grade_year', lazy='dynamic'))
+
+    def __str__(self):
+        return self.name
 
 class Students(db.Model):
     __tablename__ = 'students'
@@ -50,6 +60,9 @@ class Students(db.Model):
     birthdate = Column(Date, nullable=False)
     class_id = Column(Integer, ForeignKey(Classes.class_id), nullable=True)
     class_name = db.relationship('Classes', backref=db.backref('class', lazy='dynamic'))
+    year_id = Column(Integer, ForeignKey(EntryYear.year_id), nullable=True)
+    year_name = db.relationship('EntryYear', backref=db.backref('year', lazy='dynamic'))
+
 
     def __str__(self):
         return  self.firstname +" " +self.lastname
@@ -115,30 +128,21 @@ class Teachers(db.Model):
     def __str__(self):
         return  self.firstname +" " +self.lastname
 
-class Ftm_scores(db.Model):
-    __tablename__ = 'ftm_scores'
+class ScoreType(db.Model):
+    __tablename__ = 'score_type'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    score = Column(Float, nullable=False)
-
-    def __str__(self):
-        return str(self.score)
-
-
-class Stm_scores(db.Model):
-    __tablename__ = 'stm_scores'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    score = Column(Float, nullable=False)
+    type_id = Column(Integer, primary_key=True, autoincrement=True)
+    type_name = Column(String(50), nullable=False)
 
     def __str__(self):
-        return str(self.score)
+        return self.type_name
 
 
-class Scores(db.Model):
-    __tablename__ = 'scores'
 
-    score_id = Column(Integer, primary_key=True, autoincrement=True)
+class ScoreBoard(db.Model):
+    __tablename__ = 'score_board'
+
+    board_id = Column(Integer, primary_key=True, autoincrement=True)
     student_id = Column(Integer, ForeignKey(Students.student_id), nullable=False)
     student = db.relationship('Students', backref=db.backref('score', lazy='dynamic'))
     teacher_id = Column(Integer, ForeignKey(Teachers.teacher_id), nullable=False)
@@ -149,14 +153,28 @@ class Scores(db.Model):
     school_year = db.relationship('School_year', backref=db.backref('score', lazy='dynamic'))
     semester_id = Column(Integer, ForeignKey(Semesters.semester_id), nullable=False)
     semester = db.relationship('Semesters', backref=db.backref('score', lazy='dynamic'))
-    ftm_scores_id = Column(Integer, ForeignKey(Ftm_scores.id), nullable=False)
-    ftm_scores = db.relationship('Ftm_scores', backref=db.backref('score_s', lazy='dynamic'))
-    stm_scores_id = Column(Integer, ForeignKey(Stm_scores.id), nullable=False)
-    stm_scores = db.relationship('Stm_scores', backref=db.backref('score_s', lazy='dynamic'))
+
+class Score(db.Model):
+    __tablename__ = 'score'
+
+    score_id = Column(Integer, primary_key=True, autoincrement=True)
+    score = Column(Float, nullable=False)
+    type_id = Column(Integer, ForeignKey(ScoreType.type_id), nullable=False)
+    type = db.relationship('ScoreType', backref=db.backref('type', lazy='dynamic'))
+    board_id = Column(Integer, ForeignKey(ScoreBoard.board_id), nullable=False)
 
     def __str__(self):
-        return self.subject
+        return str(self.score)
 
+class Assignments(db.Model):
+    """phân công lớp cho giáo viên"""
+    __tablename__ = 'assignments'
+
+    assign_id = Column(Integer, primary_key=True, autoincrement=True)
+    teacher_id = Column(Integer, ForeignKey(Teachers.teacher_id), nullable=False)
+    teacher = db.relationship('Teachers', backref=db.backref('assign', lazy='dynamic'))
+    class_id = Column(Integer, ForeignKey(Classes.class_id), nullable=False)
+    class_name = db.relationship('Classes', backref=db.backref('assign', lazy='dynamic'))
 
 if __name__ == '__main__':
     db.create_all()
